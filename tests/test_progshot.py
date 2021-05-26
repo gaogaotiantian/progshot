92836f2a7025d55ee4830a0fed3bea13db8cc835
gaogaotiantian <gaogaotiantian@hotmail.com>
2021-05-25 22:05:11 -0700

    Add source line cache to improve performance. (#4)

--- tests/test_progshot.py
+++ tests/test_progshot.py
@@ -2,10 +2,13 @@
 # For details: https://github.com/gaogaotiantian/progshot/blob/master/NOTICE.txt
 
 
+import inspect
 import os
 import progshot
+from progshot.film import Film
 import tempfile
 import unittest
+from unittest.mock import patch
 from .cli_tmpl import CLITmpl
 
 
@@ -17,6 +20,21 @@ class TestProgShot(unittest.TestCase):
         self.assertTrue(os.path.exists("test.pshot"))
         self.assertTrue(os.stat("test.pshot").st_size > 0)
         os.remove("test.pshot")
+
+    def test_capture_lambda(self):
+        ps = progshot.ProgShot(save_at_exit=False)
+        _ = max([1, 2, 3], key=lambda _: ps.capture() or 1)
+        self.assertEqual(len(ps._films), 3)
+
+    def test_capture_without_filename(self):
+        def f(ps):
+            ps.capture()
+        ps = progshot.ProgShot(save_at_exit=False)
+        with patch.object(inspect, "getsourcelines", return_value=None, side_effect=OSError()) as _:
+            f(ps)
+            film = Film(ps._films[-1])
+            self.assertEqual(film.frames[0].start_lineno, -1)
+            self.assertEqual(film.frames[0].frame_lines, 0)
 
     def test_config(self):
         ps = progshot.ProgShot()
