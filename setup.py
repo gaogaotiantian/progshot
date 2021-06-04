941c63bab09cdeaebc5dd8871e9d000313908fe7
Stella Zhao <mengjia@ucsb.edu>
2021-06-04 16:47:46 -0700

    remove python 3.6

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setuptools.setup(
     },
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
