8d65ed5509ffb02b3470b8bcb4bb1b371ce5165c
Stella Zhao <mengjia@ucsb.edu>
2021-06-04 16:40:30 -0700

    change syntax

--- src/progshot/cli.py
+++ src/progshot/cli.py
@@ -231,7 +231,7 @@ class CLI:
             print(s)
 
     def _do_default(self, args):
-        self.info(f"unknown syntax {args}")
+        self.info(f"Unknown syntax {args}")
         return False
 
     def _get_val(self, val):
