c408e0bbbe5636a81c9b21886790a064c1b2038b
gaogaotiantian <gaogaotiantian@hotmail.com>
2021-06-20 16:10:55 -0700

    Add benchmarks (#12)
    * Add benchmarks

---
+++ benchmark/benchmark_cases/case_many_local.py
@@ -0,0 +1,39 @@
+# Licensed under the Apache License: http://www.apache.org/licenses/LICENSE-2.0
+# For details: https://github.com/gaogaotiantian/progshot/blob/master/NOTICE.txt
+
+
+import io
+from progshot import shoot, dump
+
+
+class BenchmarkCase:
+    def __init__(self):
+        self.s = io.StringIO()
+        self.big_string = "abc"*1000
+        self.big_dict = {f"{i}": i for i in range(20)}
+
+    def do_baseline(self):
+        self.many_local_changes()
+
+    def do_experiment(self):
+        with shoot(depth=2):
+            self.many_local_changes()
+
+    def do_dump(self):
+        films = dump()
+        return films
+
+    def many_local_changes(self):
+        a = 1
+        b = 2
+        c = 3
+        d = 4
+        e = 5
+        f = 6
+
+
+
+bm = BenchmarkCase()
+do_baseline = bm.do_baseline
+do_experiment = bm.do_experiment
+do_dump = bm.do_dump
---
+++ benchmark/run_benchmark.py
@@ -0,0 +1,38 @@
+import benchmark_cases
+import importlib
+import os
+import time
+
+
+class Benchmark:
+    def run(self):
+        for file in os.listdir(benchmark_cases.__spec__.submodule_search_locations[0]):
+            if file.startswith("case") and file.endswith(".py"):
+                self.run_case(file[:-3])
+
+    def run_case(self, module_name):
+        m = importlib.import_module(f"benchmark_cases.{module_name}")
+        if hasattr(m, "do_init"):
+            m.do_init()
+        baseline_time = self.timeit(m.do_baseline)
+        experiment_time = self.timeit(m.do_experiment)
+        films = m.do_dump()
+        pshot_size = os.stat("out.pshot").st_size
+        os.remove("out.pshot")
+        print(f"=============== {module_name} ===============")
+        print(f"= Baseline   - {baseline_time}")
+        print(f"= Experiment - {experiment_time}")
+        print(f"= Overhead   - {experiment_time / baseline_time}")
+        print(f"= Pshot Size - {pshot_size}")
+        print(f"= Films      - {films}")
+        print(f"= Byte/Film  - {pshot_size / films}")
+
+    def timeit(self, func):
+        start = time.perf_counter()
+        func()
+        return time.perf_counter() - start
+
+
+if __name__ == "__main__":
+    bm = Benchmark()
+    bm.run()
